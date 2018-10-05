ea203539603060a516ca1829c18f723d36d1185f
Peter Czech <pc@exklusiv.de>
2018-10-05 21:46:14 +0200

    Support for linear move division implemented

--- octoprint_levelpcb/__init__.py
+++ octoprint_levelpcb/__init__.py
@@ -20,7 +20,14 @@ class LevelPCBPlugin(octoprint.plugin.SettingsPlugin,
     profile = dict()
     profiles = dict()
     position_absolute = True
-    last_x = last_y = last_z = 0.0
+    last = [0.0, 0.0, 0.0, 0.0]
+    regex = [
+        re.compile('X([\-\d\.]+)', re.IGNORECASE),
+        re.compile('Y([\-\d\.]+)', re.IGNORECASE),
+        re.compile('Z([\-\d\.]+)', re.IGNORECASE),
+        re.compile('E([\-\d\.]+)', re.IGNORECASE)
+    ]
+    output = ['X%.3f', 'Y%.3f', 'Z%.3f', 'E%.3f']
 
     def on_after_startup(self):
         # load saved profiles from settings for fast access
@@ -45,6 +52,7 @@ class LevelPCBPlugin(octoprint.plugin.SettingsPlugin,
                 lift = 0,
                 lift_feed = 300,
                 fade = 2,
+                divide = 30,
                 safe_homing = False,
                 home_x = 100,
                 home_y = 100,
@@ -176,123 +184,60 @@ class LevelPCBPlugin(octoprint.plugin.SettingsPlugin,
             # calculate z-offset at given position
             # first get X/Y/Z-coordinates from command
             # this is always executed for coordinate tracking
-            match_x = re.search('X([\-\d\.]+)', cmd, re.IGNORECASE)
-            match_y = re.search('Y([\-\d\.]+)', cmd, re.IGNORECASE)
-            match_z = re.search('Z([\-\d\.]+)', cmd, re.IGNORECASE)
-            x = y = z = 0.0
-            if match_x:
-                if self.position_absolute: x = float(match_x.group(1))
-                else: x = self.last_x + float(match_x.group(1))
-            else:
-                x = self.last_x
-            if match_y:
-                if self.position_absolute: y = float(match_y.group(1))
-                else: y = self.last_y + float(match_y.group(1))
-            else:
-                y = self.last_y
-            if match_z:
-                if self.position_absolute: z = float(match_z.group(1))
-                else: z = self.last_z + float(match_z.group(1))
-            else:
-                z = self.last_z
+            match = [r.search(cmd) for r in self.regex]
+            current = [0.0 for i in range(4)]
+            for i in range(len(current)):
+                if match[i]:
+                    if self.position_absolute: current[i] = float(match[i].group(1))
+                    else: current[i] = self.last[i] + float(match[i].group(1))
 
             # check if we need to calculate a z-offset
             if len(self.profile['matrix']) == 0:
                 # store last X/Y/Z
-                self.last_x, self.last_y, self.last_z = x, y, z
+                self.last = current[:]
                 # we have no matrix, do nothing
                 return cmd
-            elif self.profile['fade'] > 0 and z > self.profile['fade']:
+            elif self.profile['fade'] > 0 and current[2] > self.profile['fade']:
                 # store last X/Y/Z
-                self.last_x, self.last_y, self.last_z = x, y, z
+                self.last = current[:]
                 # we are above the fading height, do nothing
                 return cmd
 
-            # calculate surrounding matrix points
-            dist_x = (self.profile['max_x'] - self.profile['min_x']) / float(self.profile['count_x'] - 1)
-            dist_y = (self.profile['max_y'] - self.profile['min_y']) / float(self.profile['count_y'] - 1)
-            index_x = (x - self.profile['min_x']) / dist_x
-            index_y = (y - self.profile['min_y']) / dist_y
-            # find out where the point is relative to the matrix
-            index_nearby = []
-            if x < self.profile['min_x']:
-                if y < self.profile['min_y']:
-                    # point is top left of matrix
-                    index_nearby.append([0, 0])
-                elif y > self.profile['max_y']:
-                    # point is bottom left of matrix
-                    index_nearby.append([0, self.profile['count_y'] - 1])
-                else:
-                    # point is left of matrix
-                    index_nearby.append([0, math.floor(index_y)])
-                    index_nearby.append([0, math.ceil(index_y)])
-            elif x > self.profile['max_x']:
-                if y < self.profile['min_y']:
-                    # point is top right of matrix
-                    index_nearby.append([self.profile['count_x'] - 1, 0])
-                elif y > self.profile['max_y']:
-                    # point is bottom right of matrix
-                    index_nearby.append([self.profile['count_x'] - 1, self.profile['count_y'] - 1])
-                else:
-                    # point is right of matrix
-                    index_nearby.append([self.profile['count_x'] - 1, math.floor(index_y)])
-                    index_nearby.append([self.profile['count_x'] - 1, math.ceil(index_y)])
+            if not match[2]:
+                cmd = '%s Z%.3f' % (cmd, self.last[2])
+
+            # calculate move length, subdivide if necessary
+            result = []
+            move_length = math.sqrt((self.last[0] - current[0]) ** 2 + (self.last[1] - current[1]) ** 2)
+            # self._logger.info('Move length: %.3f' % move_length)
+            if self.profile['divide'] > 0 and move_length > self.profile['divide']:
+                # move is longer than subdivision setting, split into smaller moves
+                factor = math.ceil(move_length / self.profile['divide'])
+                # calculate move lengths of segments per axis
+                lengths = [(current[i] - self.last[i]) / factor for i in range(len(current))]
+                for n in range(1, int(factor) + 1):
+                    if self.position_absolute:
+                        move_point = [self.last[i] + lengths[i] * n for i in range(len(current))]
+                    else:
+                        move_point = [lengths[i] * n for i in range(len(current))]
+                    move_point[2] += self.get_z_offset(move_point[0], move_point[1], move_point[2])
+                    result.append(self.sub_all(
+                        self.regex,
+                        [self.output[i] % move_point[i] for i in range(len(move_point))],
+                        cmd
+                    ))
             else:
-                if y < self.profile['min_y']:
-                    # point is top of matrix
-                    index_nearby.append([math.floor(index_x), 0])
-                    index_nearby.append([math.ceil(index_x), 0])
-                elif y > self.profile['max_y']:
-                    # point is bottom of matrix
-                    index_nearby.append([math.floor(index_x), self.profile['count_y'] - 1])
-                    index_nearby.append([math.ceil(index_x), self.profile['count_y'] - 1])
-                else:
-                    # point is inside matrix, use all 4 nearby points
-                    index_nearby = [
-                        [ math.floor(index_x), math.floor(index_y) ],
-                        [ math.ceil(index_x),  math.floor(index_y) ],
-                        [ math.floor(index_x), math.ceil(index_y)  ],
-                        [ math.ceil(index_x),  math.ceil(index_y)  ]
-                    ]            
-
-            # get nearby points and their distance from our wanted point
-            points_nearby = []
-            for i in index_nearby:
-                point = self.profile['matrix'][int(i[1]) * int(self.profile['count_x']) + int(i[0])]
-                distance = math.sqrt((x - point[0]) ** 2 + (y - point[1]) ** 2)
-                points_nearby.append(point + [distance])
-
-            # calculate an average z-offset by distance from all found points
-            average_z = 0.0
-            total_distance = sum(p[3] for p in points_nearby)
-            exact_matches = [p for p in points_nearby if p[3] == 0]
-            if len(exact_matches) > 0:
-                # one distance is 0, which means point matches exactly, use that value
-                average_z = exact_matches[0][2]
-            else:
-                # no distance is 0, calculate percentage factor of distance, the closer the higher
-                total_factor = sum([total_distance / p[3] for p in points_nearby])
-                for p in points_nearby:
-                    factor = total_distance / p[3] / total_factor
-                    average_z += p[2] * factor
-
-            # apply fading height factor
-            if self.profile['fade'] > 0 and z > 0:
-                average_z *= 1 - z / self.profile['fade']
-
-            # store last X/Y/Z
-            self.last_x, self.last_y, self.last_z = x, y, z
-
-            # insert calculated z-offset
-            if match_z:
-                # z-value is replaced with new value
-                return cmd[:match_z.start()] + 'Z%.3f' % (z + average_z) + cmd[match_z.end():]
-            else:
-                # z-value did not exist in command, append at the end
+                offset = self.get_z_offset(current[0], current[1], current[2])
                 if self.position_absolute:
-                    return '%s Z%.3f' % (cmd, self.last_z + average_z)
+                    # self._logger.info([current, offset])
+                    result.append(self.regex[2].sub(self.output[2] % (current[0] + offset), cmd))
                 else:
-                    return '%s Z%.3f' % (cmd, average_z)
+                    result.append(self.regex[2].sub(self.output[2] % offset, cmd))
+
+            # store last X/Y/Z and return command(s)
+            self.last = current[:]
+            # self._logger.info('New position: %s', repr(self.last))
+            return result
 
         elif gcode and gcode == 'G28' and self.profile['safe_homing']:
             commands = []
@@ -321,10 +266,87 @@ class LevelPCBPlugin(octoprint.plugin.SettingsPlugin,
             if not self.position_absolute:
                 commands.append('G91')
             # we don't know where the printer moves to, clear last coordinates
-            self.last_x = self.last_y = self.last_z = 0.0
+            self.last[0] = self.last[1] = self.last[2] = 0.0
             return commands
         elif gcode and gcode == 'G30' and self.profile['lift'] > 0:
             return ['G91', 'G0 Z%.3f' % self.profile['lift'], 'G90', cmd]
+
+    def get_z_offset(self, x, y, z):
+        if not self.position_absolute:
+            x, y, z = x + self.last[0], y + self.last[1], z + self.last[2]
+        # calculate surrounding matrix points
+        dist_x = (self.profile['max_x'] - self.profile['min_x']) / float(self.profile['count_x'] - 1)
+        dist_y = (self.profile['max_y'] - self.profile['min_y']) / float(self.profile['count_y'] - 1)
+        index_x = (x - self.profile['min_x']) / dist_x
+        index_y = (y - self.profile['min_y']) / dist_y
+        # find out where the point is relative to the matrix
+        index_nearby = []
+        if x < self.profile['min_x']:
+            if y < self.profile['min_y']:
+                # point is top left of matrix
+                index_nearby.append([0, 0])
+            elif y > self.profile['max_y']:
+                # point is bottom left of matrix
+                index_nearby.append([0, self.profile['count_y'] - 1])
+            else:
+                # point is left of matrix
+                index_nearby.append([0, math.floor(index_y)])
+                index_nearby.append([0, math.ceil(index_y)])
+        elif x > self.profile['max_x']:
+            if y < self.profile['min_y']:
+                # point is top right of matrix
+                index_nearby.append([self.profile['count_x'] - 1, 0])
+            elif y > self.profile['max_y']:
+                # point is bottom right of matrix
+                index_nearby.append([self.profile['count_x'] - 1, self.profile['count_y'] - 1])
+            else:
+                # point is right of matrix
+                index_nearby.append([self.profile['count_x'] - 1, math.floor(index_y)])
+                index_nearby.append([self.profile['count_x'] - 1, math.ceil(index_y)])
+        else:
+            if y < self.profile['min_y']:
+                # point is top of matrix
+                index_nearby.append([math.floor(index_x), 0])
+                index_nearby.append([math.ceil(index_x), 0])
+            elif y > self.profile['max_y']:
+                # point is bottom of matrix
+                index_nearby.append([math.floor(index_x), self.profile['count_y'] - 1])
+                index_nearby.append([math.ceil(index_x), self.profile['count_y'] - 1])
+            else:
+                # point is inside matrix, use all 4 nearby points
+                index_nearby = [
+                    [ math.floor(index_x), math.floor(index_y) ],
+                    [ math.ceil(index_x),  math.floor(index_y) ],
+                    [ math.floor(index_x), math.ceil(index_y)  ],
+                    [ math.ceil(index_x),  math.ceil(index_y)  ]
+                ]            
+
+        # get nearby points and their distance from our wanted point
+        points_nearby = []
+        for i in index_nearby:
+            point = self.profile['matrix'][int(i[1]) * int(self.profile['count_x']) + int(i[0])]
+            distance = math.sqrt((x - point[0]) ** 2 + (y - point[1]) ** 2)
+            points_nearby.append(point + [distance])
+
+        # calculate an average z-offset by distance from all found points
+        average_z = 0.0
+        total_distance = sum(p[3] for p in points_nearby)
+        exact_matches = [p for p in points_nearby if p[3] == 0]
+        if len(exact_matches) > 0:
+            # one distance is 0, which means point matches exactly, use that value
+            average_z = exact_matches[0][2]
+        else:
+            # no distance is 0, calculate percentage factor of distance, the closer the higher
+            total_factor = sum([total_distance / p[3] for p in points_nearby])
+            for p in points_nearby:
+                factor = total_distance / p[3] / total_factor
+                average_z += p[2] * factor
+
+        # apply fading height factor
+        if self.profile['fade'] > 0 and z > 0:
+            average_z *= 1 - z / self.profile['fade']
+
+        return average_z
 
     # set the status variable and send change to front-end
     def set_status(self, status, text):
@@ -344,6 +366,12 @@ class LevelPCBPlugin(octoprint.plugin.SettingsPlugin,
     # compares two coordinates for equality with 0.1mm tolerance
     def coords_equal(self, float1, float2):
         return abs(float1 - float2) < 0.1
+
+    def sub_all(self, search_res, replace_strs, input_str):
+        result = input_str
+        for i in range(len(search_res)):
+            result = search_res[i].sub(replace_strs[i], result)
+        return result
 
     def get_assets(self):
         return dict(
